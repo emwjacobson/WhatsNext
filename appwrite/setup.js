cff585e726fda6c209b8208a699536eccc21156c
Emerson Jacobson <emwjacobson@gmail.com>
2021-03-25 01:09:58 +0000

    Added check to see if function already exists

--- appwrite/setup.js
+++ appwrite/setup.js
@@ -11,20 +11,38 @@ client.setEndpoint(endpoint)
       .setProject(project_id)
       .setKey(secret_key);
 
+// // Create Database Collections
+// let database = new sdk.Database(client);
+
+// let classes_db_name = "classes"
+// database.createCollection(classes_db_name, ["*"], ["*"], [])
+//       .then((create_res) => {
+//             console.log("Successfully created database: " + classes_db_name);
+//       })
+//       .catch((error) => {
+//             console.log("Error creating database collection '" + classes_db_name + "'");
+//             console.log(error);
+//       });
+
+
+// Create Cloud Functions
 let functions = new sdk.Functions(client);
 
-let name = 'test_function';
-functions.create(name, [], 'node-14.5')
-      .then((func_res) => {
-            let func_id = func_res.$id;
-            return func_id, functions.createTag(func_id, 'node test.js', fs.createReadStream('hello_world.tar.gz'))
-      }).then((tag_res) => {
-            let func_id = tag_res.functionId;
-            let tag_id = tag_res.$id;
-            return functions.updateTag(func_id, tag_id)
-      }).then((update_res) => {
-            console.log("Successfully created " + name);
-      })
-      .catch((error) => {
-            console.log(error);
-      });
+let test_name = 'test_function';
+functions.list("name=" + test_name)
+.then((funcs) => {
+      if (funcs.sum == 0) {
+            return functions.create(test_name, [], 'node-14.5');
+      } else {
+            console.log("Cloud function '" + test_name + "' already exists");
+            return funcs.functions[0];
+      }
+}).then((func_res) => {
+      return functions.createTag(func_res.$id, 'node test.js', fs.createReadStream('hello_world.tar.gz'));
+}).then((tag_res) => {
+      return functions.updateTag(tag_res.functionId, tag_res.$id);
+}).then((update_res) => {
+      console.log("Successfully created function: " + test_name);
+}).catch((error) => {
+      console.log(error)
+});
