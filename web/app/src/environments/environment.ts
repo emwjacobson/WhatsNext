1584506a18436d8b1358a60be5fd597e509c331b
Emerson Jacobson <emwjacobson@gmail.com>
2021-04-01 18:15:11 +0000

    Fixed endpoint url

--- web/app/src/environments/environment.ts
+++ web/app/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   title: "Whats Next?",
-  appwrite_endpoint: "https://backend.emwj.dev",
+  appwrite_endpoint: "https://backend.emwj.dev/v1",
   appwrite_project_id: "605e5da17bdf7",
   appwrite_classes_id: "605e5dfee2b19",
   appwrite_entry_id: "605e5dfee0b44"
